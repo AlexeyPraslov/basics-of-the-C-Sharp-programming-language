98ed4459743fe311bcdca239db29eec0d2defd6b
Alexey <ipad2322012@gmail.com>
2023-12-03 14:47:09 +0300

    добавили задачу с if else

---
+++ Lessons/Lesson_1/task_5/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа приветсвия пользователя
+
+Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");//меняет кодировку с ютф8 на ютф 16
+System.Console.WriteLine("введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "таня")//туловер - принимает любой регитр букв
+{
+    System.Console.WriteLine("Ура, это же Таня");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
