30a7424307da8db32e1fa4a32430b7b8453293e2
Alexey <ipad2322012@gmail.com>
2023-12-03 14:23:01 +0300

    добавили задачу на суммирование и деление

---
+++ Lessons/Lesson_1/task_3/Program.cs
@@ -0,0 +1,6 @@
+﻿// программа суммирования 
+
+int numberA = 15;
+int numberB = 10;
+int result = numberA + numberB;
+Console.WriteLine(result);
---
+++ Lessons/Lesson_1/task_4/Program.cs
@@ -0,0 +1,6 @@
+﻿// программа деления 
+
+double numberA = 15;
+double numberB = 10;
+
+Console.WriteLine(numberA / numberB);
