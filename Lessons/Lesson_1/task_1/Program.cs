1efdf06041cbad66f76dd8df2291621133c4b6ca
Alexey <ipad2322012@gmail.com>
2023-12-03 14:03:35 +0300

    добавили вторую задачу приветсвия пользователя

--- Lessons/Lesson_1/task_1/Program.cs
+++ Lessons/Lesson_1/task_1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// первая программа приветствия мира!
+
 Console.WriteLine("Hello, World!");
---
+++ Lessons/Lesson_1/task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа приветсвия пользователя
+
+Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");   
+System.Console.Write(username);
