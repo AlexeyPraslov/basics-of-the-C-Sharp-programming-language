8a00e771854aed30a762c3e6368b4980d8c0b0c4
Alexey <ipad2322012@gmail.com>
2023-12-03 15:14:14 +0300

    добавили задачу которая рисует и у меня не работает

---
+++ Lessons/Lesson_1/task_7/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача с рисованием
+//Console.clear(); //стирает консоль каждый запуск программы
+//Console.SetCursorPosition(10, 4);
+//System.Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 40, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
