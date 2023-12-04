3a8d4017334864012b49982e4ca5abe681d939f9
Alexey <ipad2322012@gmail.com>
2023-12-04 15:41:23 +0300

    что надо добавили, что не надо удалили

---
+++ Lessons/Lesson_2_Arrays/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// найти в одномерном массиве значение равное find
+
+int[] array = {12, 2, 32, 6, 4, 23, 43, 53};
+
+int n = array.Length;//Lenght - возвращат длинну массива в n
+
+int find = 43;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+    }
+    index++;//index = inde + 1
+}
