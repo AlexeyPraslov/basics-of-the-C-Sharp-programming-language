8a9dd4de230540f07e427e91aa5a04a1efab6f28
Alexey <ipad2322012@gmail.com>
2023-12-04 17:56:44 +0300

    добавили еще задачу со 2 семинара

---
+++ Seminars/Seminar_2_algorithms/task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите программу которая принимае на вход трехзначное 
+// число и удаляет вторую цифру этого числа
+// a = 256 = 26
+
+System.Console.WriteLine("Введите трехзнаное чиcло: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int first = number / 100; //получаем первую цифру числа
+    int second = number % 10; //получаем третью цифру числа
+    System.Console.WriteLine(first + " " + second);
+}
+else
+{
+    System.Console.WriteLine("введено не трехзначное число");
+}
---
+++ Seminars/Seminar_2_algorithms/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программу которая принимает на вход трехзначное число и 
+// возводит второе число в степень равную третьему числу
+
+System.Console.WriteLine("введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+    System.Console.WriteLine("некорректный ввод");
+    return;
+}
+
+int third = number % 10;
+int second = number / 10 % 10;
+int pow = 1;
+
+while(third != 0)
+{
+    pow = pow * second;
+    --third; // third = third - 1
+}
+
+System.Console.WriteLine($"результат возведения в степень {pow}");
