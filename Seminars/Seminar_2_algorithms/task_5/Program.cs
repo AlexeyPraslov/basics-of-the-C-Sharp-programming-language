04b2210ab60f39e8ee41a8f4ba2a526ddac59f3b
Alexey <ipad2322012@gmail.com>
2023-12-05 10:49:23 +0300

    сделали классную работу со 2 семинара. приступаем к домашке

---
+++ Seminars/Seminar_2_algorithms/task_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа которая выводит третью цифру с конца, или пишет, что третей цифры нет
+
+System.Console.WriteLine("введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num > 999)
+{
+    System.Console.WriteLine("число не трехзначное");
+    return;
+}
+
+int lastNum = num % 10;
+
+System.Console.WriteLine($"последняя цифра числа {num} - {lastNum}");
